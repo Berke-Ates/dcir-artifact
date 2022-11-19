95d779ed40c42ddfb198b0946c5a19cf331e3848
Berke Ates <beates@student.ethz.ch>
2022-11-19 22:12:44 +0100

    added run_all

---
+++ scripts/run_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Desc: Runs all the benchmarks. The output contains any intermediate results 
+# and the times in the CSV format as well as all the plots.
+# Usage: ./run_all.sh <Output Dir> <Repetitions>
+
+# Check args
+if [ $# -ne 2 ]; then
+  echo "Usage: ./run_all.sh <Output Dir> <Repetitions>"
+  exit 1
+fi
+
+# Read args
+output_dir=$1
+repetitions=$2
+
+# Helpers
+scripts_dir=$(dirname $0)
+
+# Run benchmarks
+./$scripts_dir/Polybench/run_all.sh "$output_dir" "$repetitions"
+./$scripts_dir/pytorch/run_all.sh "$output_dir" "$repetitions"
+./$scripts_dir/snippets/run_all.sh "$output_dir" "$repetitions"
