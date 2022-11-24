b93fd1c64e50d1f3e219be1697a30dcf7c379895
Berke Ates <beates@student.ethz.ch>
2022-11-24 13:22:01 +0100

    added dace scripts

---
+++ scripts/Polybench/bench_dace.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+# Desc: Runs a Polybench SDFG benchmark for DaCe
+# Usage: ./bench_sdfg.py <Input SDFG> <Repetitions>
+
+import sys
+import dace
+
+if len(sys.argv) != 3:
+    print("DaCe Polybench Benchmarking Tool")
+    print("Arguments:")
+    print("  Input SDFG: The SDFG to benchmark")
+    print("  Repetitions: How many times to run the benchmark")
+    exit(1)
+
+input_file = sys.argv[1]
+repetitions = int(sys.argv[2])
+
+# Load and compile SDFG
+sdfg = dace.SDFG.from_file(input_file)
+obj = sdfg.compile()
+
+# Run Benchmark
+for i in range(repetitions):
+    argv_loc = dace.ndarray(shape=(0, ), dtype=dace.dtypes.int8)
+    obj(argc_loc=0, _argcount=0, argv_loc=argv_loc)
